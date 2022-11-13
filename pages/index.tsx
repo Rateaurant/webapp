ef88f0757ad3280a2cd15e6b2888ef3ef672b1ce
FloatingComet62 <shubh2007rai@gmail.com>
2022-11-13 15:14:20 +0530

    Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,21 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
+import Head from "../components/Head";
 import styles from "../styles/pages/Home.module.scss";
+import Button from "../components/Home/Button";
+import Base from "../components/Base";
 
 export default function Index() {
   return (
     <>
-      <Head>
-        <title>Restaurant Name</title>
-      </Head>
-      <Image
-        src={require("public/bg.svg")}
-        alt="Background"
-        className={styles.image}
-        fill
-      />
+      <Base />
       <section className={styles.body}>
-        <div>
-          <div>
-            <h1>Name of the restaurant</h1>
-            <h2>powered by Rateaurant</h2>
-          </div>
-        </div>
+        <Head />
         <article className={styles.description}>
           A short description of the restaurant
         </article>
-        <article className={styles.buttons}>
-          <div>
-            Rating and Reviews
-            <Link href="/review">view</Link>
-          </div>
-          <div>
-            Order Now!
-            <Link href="/order">
-              <Image
-                src={require("public/next.svg")}
-                alt="Go"
-                width={50}
-                height={50}
-              />
-            </Link>
-          </div>
-        </article>
+        <section className={styles.buttons}>
+          <Button label="Ratings" />
+          <Button label="Menu" />
+        </section>
       </section>
     </>
   );
