e1c226cee19ba0f5e667dbbad137859da29b621f
FloatingComet62 <shubh2007rai@gmail.com>
2022-11-15 21:36:32 +0530

    Menu page

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -1,3 +1,36 @@
-export default function Order() {
-  return <h1>Order</h1>;
+import Head from "../components/Head";
+import Base from "../components/Base";
+import styles from "../styles/pages/Menu.module.scss";
+import Item from "../components/Menu/Item";
+
+export default function Menu() {
+  return (
+    <>
+      <Base />
+      <section className={styles.body}>
+        <Head />
+      </section>
+      <section className={styles.items}>
+        <Item
+          name="Burger with fries"
+          label="1"
+          description="food description for some weird chinese food no one knows about or something"
+          price={19.99}
+          offer={{
+            active: true,
+            newPrice: 14.99,
+          }}
+        />
+        <Item
+          name="Noodles"
+          label="2"
+          description="it's fucking noodles, you know what it is"
+          price={13.99}
+          offer={{
+            active: false,
+          }}
+        />
+      </section>
+    </>
+  );
 }
--- pages/ratings.tsx
+++ pages/ratings.tsx
@@ -1,3 +1,14 @@
-export default function Review() {
-  return <h1>Review</h1>;
+import Head from "../components/Head";
+import Base from "../components/Base";
+import styles from "../styles/pages/Ratings.module.scss";
+
+export default function Ratings() {
+  return (
+    <>
+      <Base />
+      <section className={styles.body}>
+        <Head />
+      </section>
+    </>
+  );
 }
