021827a27fe5c95f86784816da60526fb873da2a
FloatingComet62 <shubh2007rai@gmail.com>
2023-01-18 17:57:01 +0530

    Reset :)

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+</script>
+
+<h1 class="text-3xl font-bold underline text-blue-400">Hello World</h1>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.zinc.900);
+	}
+</style>
