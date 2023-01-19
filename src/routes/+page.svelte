40a7059aef2d24688e48004a8719645a06ac9d7c
FloatingComet62 <shubh2007rai@gmail.com>
2023-01-19 11:22:01 +0530

    Aligning and Gradients

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,43 +2,48 @@
 	import '../app.css';
 </script>
 
-<img src="/bg.png" alt="" class="absolute h-[100vh] w-[100%] z-[-1]" />
-<main class="text-center text-white text-5xl p-8">Name of the restaurant</main>
-<div class="flex flex-row justify-center">
-	<h2 class="text-center text-white text-3xl p-4">powered by</h2>
-	<img src="/brandingLogo.png" alt="" class="w-7 h-7 self-center" />
-	<h2 class="text-center text-white text-3xl p-4">Rateaurant</h2>
-</div>
-<h2 class="text-center text-white p-[4rem]">Short Description of the Restaurant</h2>
+<body class="flex flex-col align-middle bg-gradient-to-b from-black via-[#000000ca] to-transparent">
+	<img src="/bg.png" alt="" class="absolute h-[100vh] w-[100%] z-[-1]" />
+	<header class="flex flex-col w-fit self-center">
+		<main class="text-center text-white text-5xl pt-8">Name of the restaurant</main>
+		<div class="flex flex-row justify-center w-fit self-end">
+			<h2 class="text-center text-white font-extrabold text-3xl p-1">powered by</h2>
+			<img src="/brandingLogo.png" alt="" class="w-7 h-7 self-center" />
+			<h2 class="text-center text-white font-extrabold text-3xl p-1">Rateaurant</h2>
+		</div>
+		<h2 class="text-center text-white p-[4rem]">Short Description of the Restaurant</h2>
+	</header>
 
-<div class="flex flex-col sm:flex-row gap-16 sm:gap-8 justify-evenly">
-	<div class="box grid place-items-center">
-		<div class="flex flex-col z-50">
-			<img
-				src="/ratingIcon.svg"
-				alt=""
-				class="w-10 self-center buttonIcon transition-all duration-1000"
-			/>
-			<caption class="text-lg text-white buttonCaption transition-all duration-1000">Rating</caption
-			>
+	<div class="flex flex-col sm:flex-row gap-16 sm:gap-8 justify-evenly">
+		<div class="box grid place-items-center">
+			<div class="flex flex-col z-50">
+				<img
+					src="/ratingIcon.svg"
+					alt=""
+					class="w-10 self-center buttonIcon transition-all duration-1000"
+				/>
+				<caption class="text-lg text-white buttonCaption transition-all duration-1000"
+					>Rating</caption
+				>
+			</div>
+			<div class="cover z-30 transition-all duration-1000 rounded-2xl" />
+			<img src="/rating.png" alt="" class="buttonIMG absolute rounded-2xl w-[200px]" />
 		</div>
-		<div class="cover z-30 transition-all duration-1000 rounded-2xl" />
-		<img src="/rating.png" alt="" class="buttonIMG absolute rounded-2xl w-[200px]" />
-	</div>
-	<div class="box grid place-items-center">
-		<div class="flex flex-col z-50">
-			<img
-				src="/menuIcon.svg"
-				alt=""
-				class="w-10 self-center buttonIcon transition-all duration-1000"
-			/>
-			<caption class="text-lg text-white buttonCaption transition-all duration-1000">Rating</caption
-			>
+		<div class="box grid place-items-center">
+			<div class="flex flex-col z-50">
+				<img
+					src="/menuIcon.svg"
+					alt=""
+					class="w-10 self-center buttonIcon transition-all duration-1000"
+				/>
+				<caption class="text-lg text-white buttonCaption transition-all duration-1000">Menu</caption
+				>
+			</div>
+			<div class="cover z-30 transition-all duration-1000 rounded-2xl" />
+			<img src="/menu.png" alt="" class="buttonIMG absolute rounded-2xl w-[200px]" />
 		</div>
-		<div class="cover z-30 transition-all duration-1000 rounded-2xl" />
-		<img src="/menu.png" alt="" class="buttonIMG absolute rounded-2xl w-[200px]" />
 	</div>
-</div>
+</body>
 
 <style lang="postcss">
 	.box {
