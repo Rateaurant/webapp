b9e6419ecc9170d7975917cc7d11fcbf1e552820
FloatingComet62 <shubh2007rai@gmail.com>
2024-06-05 17:41:05 +0530

    Prettier + Tailwind + Hello World + background glitter

---
+++ src/routes/(public)/+page.svelte
@@ -0,0 +1,12 @@
+<body> Hello World </body>
+
+<style>
+	body {
+		width: 100vw - 1px;
+		height: 100vh - 1px;
+		background-image: url('/background.svg');
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-size: 100% 100%;
+	}
+</style>
