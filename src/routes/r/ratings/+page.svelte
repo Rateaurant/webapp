8598917b06d8082406fe70c2dfd66f578339e124
FloatingComet62 <shubh2007rai@gmail.com>
2024-06-05 16:39:58 +0530

    restart :)

--- src/routes/r/ratings/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import Review from '../../../lib/review.svelte';
-</script>
-
-<div class="flex flex-col w-[70%] text-white gap-3">
-	<Review username="User1" color="green" review="Loved it" star={4.5} />
-	<Review username="User2" color="red" review="Hated it" star={3} />
-</div>
