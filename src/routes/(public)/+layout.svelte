f17a35abb869ab26f0ed8ed8baa52f95a49e82fe
FloatingComet62 <shubh2007rai@gmail.com>
2024-06-07 20:02:07 +0530

    made the navbar smaller

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -3,21 +3,21 @@
 </script>
 
 <body class="bg-dark-25 text-whitetext font-montserrat">
-	<div aria-label="Header" class="flex justify-between items-center m-16">
-		<div aria-label="Navigation" class="flex gap-20 text-4xl justify-between">
+	<div aria-label="Header" class="flex justify-between items-center m-12">
+		<div aria-label="Navigation" class="flex gap-16 text-2xl justify-between">
 			<a href="/about" aria-label="About us">About us</a>
 			<a href="/contact" aria-label="Contact us">Contact us</a>
 			<a href="/faq" aria-label="Frequently asked questions">FAQs</a>
 		</div>
 		<div
 			aria-label="Authentication"
-			class="flex gap-12 text-3xl justify-between">
+			class="flex gap-12 text-xl justify-between">
 			<a
-				class="border p-4 hover:text-dark-25 hover:bg-whitetext
+				class="border p-2 hover:text-dark-25 hover:bg-whitetext
 				transition-all duration-200"
 				href="/signin">Sign In</a>
 			<a
-				class="border p-4 hover:text-dark-25 hover:bg-whitetext
+				class="border p-2 hover:text-dark-25 hover:bg-whitetext
 				transition-all duration-200"
 				href="/signup">Sign Up</a>
 		</div>
