a7bc708072ab33f7de860ace0d21891066d98d65
FloatingComet62 <shubh2007rai@gmail.com>
2024-07-01 00:07:49 +0530

    Alert spacing fix

--- src/lib/components/Alert.svelte
+++ src/lib/components/Alert.svelte
@@ -13,6 +13,7 @@
 
 	let is_sm: boolean;
 	let width: number;
+	let buttonOffsetWidth: number;
 	$: is_sm = width < 640;
 </script>
 
@@ -22,13 +23,30 @@
 	<div
 		aria-label={message}
 		class="flex justify-between text-sm sm:text-xl items-center gap-5 bg-primary pt-3 pb-3 rounded-xl border-primary border-2 bg-opacity-50">
-		<Button class="ml-4" handler={() => (show = false)}>
-			<img src={close} alt="Close alert" width={24} />
-		</Button>
+		<!-- https://stackoverflow.com/questions/68077135/how-do-i-get-offset-width-of-a-div-in-svelte -->
+		<div bind:offsetWidth={buttonOffsetWidth}>
+			<Button
+				label="Click is button to close the alert"
+				class="ml-4"
+				handler={() => (show = false)}>
+				<img src={close} alt="Close alert" width={24} />
+			</Button>
+		</div>
 		<div class="flex items-center gap-5">
 			<img src={alert} alt="Alert" width={is_sm ? 24 : 48} />
 			{message}
 		</div>
-		<div></div>
+
+		{#if is_sm}
+			<!-- Centering a fixed amount on small screen -->
+			<div aria-label="Spacing; Ignore" class="min-w-4"></div>
+		{:else}
+			<!-- Centering the message -->
+			<!-- https://stackoverflow.com/questions/4171286/how-to-make-a-div-with-no-content-have-a-width -->
+			<div
+				aria-label="Spacing; Ignore"
+				style={`min-width: ${buttonOffsetWidth}px`}>
+			</div>
+		{/if}
 	</div>
 {/if}
